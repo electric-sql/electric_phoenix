c6f4499abb130207fa7d629fc6fd704f6f508c5d
Garry Hill <garry@electric-sql.com>
2024-11-19 14:58:19 +0000

    use electric client 0.2.1-rc-*

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Electric.Phoenix.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:electric_client, "~> 0.2.0"},
+      {:electric_client, "> 0.2.0"},
       {:nimble_options, "~> 1.1"},
       {:phoenix_live_view, "~> 0.20"},
       {:plug, "~> 1.0"},
